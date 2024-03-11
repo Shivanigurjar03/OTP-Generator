21918d5afe47ebf45f30f8a0f66eb93b696ea0be
Shivanigurjar03 <shivanigurjar308@gmail.com>
2024-03-11 11:57:18 +0530

    OTP-Generajor-javascript

---
+++ secondtask.js
@@ -0,0 +1,10 @@
+let generateOTP = () => {
+    const otpLength = 6;
+
+    const otp = Math.floor(Math.random() * Math.pow(10, otpLength));
+
+    document.getElementById("otpDisplay").innerText = otp;
+};
+
+document.getElementById("generateBtn").addEventListener("click",generateOTP);
+window.addEventListener("load",generateOTp);
